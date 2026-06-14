fac2d870adaf946b6959e5211373a7383060b9d7
Saravana Kumar <saravanakumard02@gmail.com>
2026-06-14 15:49:38 +0530

    Simple version of calculator with addition, subtraction, & multiplication

---
+++ version_1.py
@@ -0,0 +1,11 @@
+def calculate(a,b,operation):
+    if operation == '+':
+        return a+b
+    elif operation == '-':
+        return a-b
+    elif operation == '*':
+        return a*b
+    
+print(calculate(2,5,'*'))
+
+
